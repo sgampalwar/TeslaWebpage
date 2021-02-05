b0d1f6a72594adec647bc975ff5434ba7470cc92
Swapnil <swapg112@gmail.com>
2021-02-05 19:02:56 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import './App.css';
+import Header from './components/Header';
+import Accessories from './assets/Desktop-Accessories.jpg'
+import ModelS from './assets/Desktop-ModelS.jpeg'
+import Model3 from './assets/Desktop-Model3.jpeg'
+import ModelX from './assets/Desktop-ModelX.jpeg'
+import ModelY from './assets/Desktop-ModelY.jpeg'
+import SolarPanels from './assets/Desktop-SolarPanels.jpeg'
+import SolarRoof from './assets/Desktop-SolarRoof.jpeg'
+import Item from './components/Item';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="app__itemsContainer">
+
+        <Item
+          title='Lowest Cost Solar Panels in America'
+          desc='Money-back Guarantee'
+          descLink=''
+          backgroundImg={SolarPanels}
+          leftBtnTxt='ORDER NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+          first
+
+        />
+        <Item
+          title='Model S'
+          desc='Order Online for Touchless Delivery'
+          descLink=''
+          backgroundImg={ModelS}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+
+        />
+        <Item
+          title='Model 3'
+          desc='Order Online for Touchless Delivery'
+          descLink=''
+          backgroundImg={Model3}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+
+        />
+        <Item
+          title='Model X'
+          desc='Order Online for Touchless Delivery'
+          descLink=''
+          backgroundImg={ModelX}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+
+        />
+        <Item
+          title='Model Y'
+          desc='Order Online for Touchless Delivery'
+          descLink=''
+          backgroundImg={ModelY}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+
+        />
+        <Item
+          title='Solar for New Roofs'
+          desc='Solar Roof Costs Less Than a New Roof Plus Solar Panels'
+          descLink=''
+          backgroundImg={SolarRoof}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+
+        />
+        <Item
+          title='Accessories'
+          desc='$69_420'
+          descLink=''
+          backgroundImg={Accessories}
+          leftBtnTxt='SHOP NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+
+        />
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
